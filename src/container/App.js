bf62354cc4accce3baf4c568f6dcb916de047c7e
gallarinkevinm@gmail.com <gallarinkevinm@gmail.com>
2018-10-02 03:16:19 +0800

    init

---
+++ src/container/App.js
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from "react";
+
+// components
+import Home from "components/Home";
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <p>I am APP</p>
+        <Home />
+      </Fragment>
+    );
+  }
+}
+
+export default App;
