31506a5549a898235aa9e37cbd4b807c2ce76eba
gallarinkevinm@gmail.com <gallarinkevinm@gmail.com>
2018-10-02 18:35:14 +0800

    store setup

--- src/Root.js
+++ src/Root.js
@@ -1,40 +1,24 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import reduxPromise from "redux-promise";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
-import reducers from "reducers/";
+import rootReducer from "reducers";
 
 let composeEnhancers = compose;
 let middlewares = [thunk, reduxPromise];
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV !== "production") {
   middlewares = [thunk, reduxPromise, logger];
   composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 }
 
-// const Store = props => {
-//   return createStore(
-//     reducers,
-//     props.initState,
-//     composeEnhancers(applyMiddleware(...middlewares))
-//   );
-// };
-
-// let store = Store();
-
-export default ({ child, initState = {} }) => {
-  return (
-    <Provider
-      store={createStore(
-        reducers,
-        initState,
-        composeEnhancers(applyMiddleware(...middlewares))
-      )}
-    >
-      {child}
-    </Provider>
+export default ({ children, initState = {} }) => {
+  let store = createStore(
+    rootReducer,
+    initState,
+    composeEnhancers(applyMiddleware(...middlewares))
   );
+  return <Provider store={store}>{children}</Provider>;
 };
