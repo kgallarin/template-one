9cdfc9dc9e34076023d668ceb14d7c4cc3944d23
gallarinkevinm@gmail.com <gallarinkevinm@gmail.com>
2018-10-10 22:00:26 +0800

    undecided

--- src/blocks/About/Inner.js
+++ src/blocks/About/Inner.js
@@ -1,15 +1,10 @@
 import styled from "styled-components";
 import Grid from "@material-ui/core/Grid";
-import baseTheme from "styles/ThemeConfig";
-import styles from "@material-ui/core/styles";
 
 const Inner = styled(Grid)`
   && {
     max-width: 960px;
     margin: 0 auto;
-    ${[baseTheme.breakpoints.down("sm")]}: {
-      padding: 0 50px;
-    };
   }
 `;
 
--- src/blocks/About/ItemListInner.js
+++ src/blocks/About/ItemListInner.js
@@ -1,7 +1,12 @@
 import styled from "styled-components";
+import { withStyles } from "@material-ui/core/styles";
 
 const ItemListInner = styled.div`
   ${"" /* padding: 50px; */};
 `;
 
-export default ItemListInner;
+export default withStyles(({ palette, spacing }) => ({
+  root: {
+    background: "#000 !important"
+  }
+}))(ItemListInner);
